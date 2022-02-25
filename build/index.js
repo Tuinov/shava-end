848b4291790dec9773bc15fcb27402ddd1993cd9
Tuinov <tuinovjob@gmail.com>
2022-02-25 21:00:12 +0300

    build

---
+++ build/index.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+// import cookieSession from 'cookie-session';
+var express_session_1 = __importDefault(require("express-session"));
+var AppRouter_1 = require("./AppRouter");
+require("./controllers/LoginController");
+require("./controllers/ProductController");
+require("./controllers/OrderController");
+var body_parser_1 = require("body-parser");
+require("dotenv/config");
+var path_1 = __importDefault(require("path"));
+var app = (0, express_1.default)();
+app.use((0, body_parser_1.json)());
+app.use(express_1.default.urlencoded({ extended: true }));
+// app.use(cookieSession({ keys: ['asfadsfadfs'] }));
+app.use((0, express_session_1.default)({
+    secret: 'sjsjkfrjkfkrjf',
+    resave: true,
+    saveUninitialized: false,
+}));
+app.use(AppRouter_1.AppRouter.getInstance());
+app.use(express_1.default.static(path_1.default.resolve(__dirname, 'client/build')));
+app.get('*', function (req, res) {
+    res.sendFile(path_1.default.resolve(__dirname, 'client/build', 'index.html'));
+});
+var PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+    console.log("Listening on port ".concat(PORT));
+});
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express, { Request, Response} from 'express';
+// import cookieSession from 'cookie-session';
+import expressSession from 'express-session'
+import { AppRouter } from './AppRouter';
+import './controllers/LoginController';
+import './controllers/ProductController';
+import './controllers/OrderController';
+import bodyParser, { json } from 'body-parser';
+import 'dotenv/config'
+import path from 'path'
+
+const app = express();
+
+app.use(json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cookieSession({ keys: ['asfadsfadfs'] }));
+app.use(
+  expressSession({
+    secret: 'sjsjkfrjkfkrjf',
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+
+app.use(AppRouter.getInstance());
+
+app.use(express.static(path.resolve(__dirname, 'client/build'))) 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
+}) 
+
+let PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
